5eb3a8207c982058a43d5d0d6b2103c517deaa4f
elfdoom <elfdao@gmail.com>
2013-09-15 21:29:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtypeNew::Application.config.secret_key_base = '5db1df4f4bb0afe21b593753bc55c7c0169a88fa4fb2b868dbcf56bf19d4d9557c743ed464bcef4953b37427ab95aeefc16e2359f3bb47cce2eec29a9c32cf44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EtypeNew::Application.config.session_store :cookie_store, key: '_etype-new_session'
